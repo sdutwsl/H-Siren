15ff23db1ff07d50579c0bd6d68c689f86cd5976
sdutwsl <sdutwsl@gmail.com>
2022-07-05 14:54:44 +0800

    测试问题

--- inc/siren-update.php
+++ inc/siren-update.php
@@ -116,7 +116,8 @@ function get_random_qiniu_pic_url($ak, $pk, $bk, $ph)
             array_push($pics, $key);
         }
     }
-    return $pics[array_rand($pics)];
+    // return $pics[array_rand($pics)];
+    return "uploads/wallpapers/landscape/元旦贺图 1920x1080.jpg";
 }
 
 /**
